5c30462bb190b4a0ab3ea3674a63c020e2da6b51
beqa eqseulidze <beqaeqseulidze6@gmail/com>
2024-03-21 23:33:40 +0400

    change code

--- src/index.js
+++ src/index.js
@@ -11,25 +11,150 @@ const getValutoData = require('./valuto');
 const getGiroData = require('./giro');
 const getRicoData = require('./rico');
 const app = express();
-
+let allData;
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
+    console.log('start get method');
     const valuto = await getValutoData();
+    console.log('valuto data:',valuto);
     const crystal = await getCrystalData();
+    console.log('crystal data:',crystal);
     const giro = await getGiroData();
+    console.log('giro data:',giro);
     const rico = await getRicoData();
-    res.render('index', {
-        allData: [{ valuto }, { crystal }, { giro }, { rico }]
-    })
+    console.log('rico data:',rico);    
+    allData = [{ valuto }, { crystal }, { giro }, { rico }];
+    const html=renderPage(allData);
+    res.type('html').send(html)    ;
 });
 
-const PORT = 4000;
-app.listen(PORT, () => {
-    console.log(`app listen port: ${PORT}...`)
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`app listen port: ${port}...`)
 
 })
 
+function getTopHtml() {
+    return `
+    <!DOCTYPE html>
+        <html lang="en">
 
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Currency App</title>
+            <style>
+                *{
+                    margin:0;
+                    padding: 0;
+                    box-sizing: border-box;
+                }
+                h1{
+                    text-align: center;
+                    margin: 20px;
+                }
+                h2{
+                    margin: 10px;
+                }
+                .cards{
+                    display: flex;
+                    flex-wrap: wrap;  
+                    gap: 10px;   
+                    justify-content: center;                
+                }
+                .card{
+                    width:300px;
+                    display:flex;
+                    flex-direction: column;
+                    align-items: center;           
+                }
+                table,tr,td,th{
+                    border-collapse: collapse;
+                    border:1px solid black;
+                }
+                table{
+                    width:100%;     
+                    font-size: 20px;                   
+                }
+                tr{
+                    cursor: pointer;
+                }
+                tr:hover{
+                    background-color: rgb(172, 155, 155);
+                }
+                    
+                    
 
+            </style>
+        </head>
+
+        <body>
+            <h1>Currencies</h1>
+            <div class="cards">    
+    `
+}
+function getBottomHtml() {
+    return `
+            </div>
+
+            <script>
+                setInterval(()=>{
+                    window.location.reload()
+                },(5*60*1000))
+            </script>
+        </body>
+
+        </html>
+    `
+}
+
+function getTableRows(currencies) {
+    let result = ''
+    for (let currency of currencies) {
+        result += `
+                <tr>
+                    <td>
+                        ${currency.ISO}              
+                    </td>
+                    <td>
+                        ${(+currency.AMOUNT_BUY).toFixed(4)}                
+                    </td>
+                    <td>
+                        ${(+currency.AMOUNT_SELL).toFixed(4)}  
+                    </td>
+                </tr>
+            `
+    }
+    return result
+}
+
+function getMiddleHtml(allData) {
+    let result = '';
+    for (let company of allData) {
+        result += `
+                    <div class="card">
+                    <h2>${Object.keys(company)[0]}</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Currency</th>
+                                <th>buy</th>
+                                <th>sell</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        ${getTableRows(Object.values(company)[0])}
+                        </tbody>
+                    </table>
+                </div> 
+                `
+    }
+
+    return result
+}
+
+function renderPage(allData){
+    return getTopHtml() + getMiddleHtml(allData) + getBottomHtml();
+}
 
